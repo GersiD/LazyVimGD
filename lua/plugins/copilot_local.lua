971291e3915450bd4223d18761170cd28da7f7cb
GD333 <gd1064@wildcats.unh.edu>
2023-04-01 23:14:33 -0400

    Improved copilot loading, and auto auth on fresh install

--- lua/plugins/copilot_local.lua
+++ lua/plugins/copilot_local.lua
@@ -1,13 +1,19 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        auto_trigger = true,
-        keymap = { accept = "<Right>" },
-      },
-    })
-  end,
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = { accept = "<Right>" },
+        },
+        panel = {
+          enabled = false,
+        },
+      })
+    end,
+  },
 }
