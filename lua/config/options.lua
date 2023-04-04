02c1993708c722dba22a6087993d7912588c7719
GD333 <gd1064@wildcats.unh.edu>
2023-04-04 09:12:56 -0400

    set localleader to \

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.maplocalleader = "\\"
 vim.opt.winbar = "%=%m %f %r %h%w"
 vim.g.clipboard = {
   name = "win32yank", -- set clipboard provider
