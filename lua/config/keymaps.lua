8e6405da6b7a13e6d09cadcb49550aa956a8f78b
GD333 <gd1064@wildcats.unh.edu>
2023-04-04 09:12:46 -0400

    fix comment keymaps :/

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,8 @@ vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<cr>", { desc = "Prev buff
 vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>hh", "<cmd>GrapplePopup tags<cr>", { desc = "Harpoon View" })
 vim.keymap.set("n", "<leader>ha", '<cmd>lua require("grapple").toggle()<cr>', { desc = "Harpoon View" })
-vim.keymap.set("v", "<leader>/", "gc", { desc = "Comment Selection" })
-vim.keymap.set("n", "<leader>/", "Vgc", { desc = "Comment Line" })
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Neotree" })
 vim.keymap.set("n", "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find in buffer" })
+
+vim.api.nvim_set_keymap("v", "<leader>/", "gc", { desc = "Comment Selection" })
+vim.api.nvim_set_keymap("n", "<leader>/", "Vgc", { desc = "Comment Line" })
