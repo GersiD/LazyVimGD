5a9464253edbb543c7f5116b365386239853fc74
Gersi.doko <Gersi.doko@unh.edu>
2023-04-04 16:39:44 -0400

    fix clipboard (kinda)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,22 +3,38 @@
 -- Add any additional options here
 vim.g.maplocalleader = "\\"
 vim.opt.winbar = "%=%m %f %r %h%w"
-vim.g.clipboard = {
-  name = "win32yank", -- set clipboard provider
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf", -- copy to clipboard
-    ["*"] = "win32yank.exe -i --crlf", -- copy to clipboard
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf", -- paste from clipboard
-    ["*"] = "win32yank.exe -o --lf", -- paste from clipboard
-  },
-}
 
 vim.g.nofsync = true
 vim.g.python_host_skip_check = true
 vim.g.python3_host_skip_check = true
-vim.g.python3_host_prog = "~/scoop/apps/python/current/python.exe"
+if jit.os == "Windows" then
+  vim.g.python3_host_prog = "~/scoop/apps/python/current/python.exe"
+  vim.g.clipboard = {
+    name = "win32yank", -- set clipboard provider
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf", -- copy to clipboard
+      ["*"] = "win32yank.exe -i --crlf", -- copy to clipboard
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf", -- paste from clipboard
+      ["*"] = "win32yank.exe -o --lf", -- paste from clipboard
+    },
+  }
+else
+  -- vim.g.clipboard = {
+  --   name = "xclip",
+  --   copy = {
+  --     ["+"] = "xclip -selection clipboard",
+  --     ["*"] = "xclip -selection clipboard",
+  --   },
+  --   paste = {
+  --     ["+"] = "xclip -selection clipboard -o",
+  --     ["*"] = "xclip -selection clipboard -o",
+  --   },
+  --   cache_enabled = true,
+  -- }
+  vim.g.clipboard = "unnamedplus"
+end
 vim.g.ruby_host_skip_check = true
 vim.g.perl_host_skip_check = true
 vim.g.loaded_ruby_provider = 0
