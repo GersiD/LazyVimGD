7aa88fd1c56f73ef5c65f011bd563690c37b0f03
GD333 <gd1064@wildcats.unh.edu>
2023-04-04 09:13:06 -0400

    setup vimtex

---
+++ lua/plugins/lsp/latex_settings.lua
@@ -0,0 +1,26 @@
+require("lspconfig.server_configurations.texlab").default_config.settings = {
+  texlab = {
+    chktex = {
+      onEdit = false,
+      onOpenAndSave = true,
+    },
+  },
+}
+require("lspconfig.server_configurations.ltex").default_config.settings = {
+  ltex = { checkFrequency = "save" },
+}
+return {
+  "lervag/vimtex",
+  lazy = false,
+  config = function()
+    vim.g.vimtex_compiler_latexmk = { build_dir = "build" }
+    vim.g.vimtex_quickfix_open_on_warning = false
+    vim.g.vimtex_quickfix_ignore_filters = { "Underfull", "Overfull" }
+    return {
+      ["vimtex_view_general_viewer"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
+      ["vimtex_view_method"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
+      ["vimtex_view_general_options"] = "-reuse-instance -forward-search @tex @line @pdf",
+      ["vimtex_view_general_options_latexmk"] = "-reuse-instance",
+    }
+  end,
+}
