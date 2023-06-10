5bf7f3d46cac21ea919b8c3665748f4d1b80bc0b
git <gd1064@wildcats.unh.edu>
2023-06-09 21:10:18 -0400

    adding python

--- lua/config/utils/terminals.lua
+++ lua/config/utils/terminals.lua
@@ -3,6 +3,10 @@ local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 local bottom = Terminal:new({ cmd = "bottom", hidden = true })
 local gdu = Terminal:new({ cmd = "gdu", hidden = true })
 local float_term = Terminal:new({ cmd = "zsh", hidden = true })
+if jit.os == "Windows" then
+  float_term = Terminal:new({ cmd = "pwsh", hidden = true })
+end
+local python_term = Terminal:new({ cmd = "python", hidden = true })
 return {
   lazygit = function()
     lazygit:toggle()
@@ -15,5 +19,8 @@ return {
   end,
   float = function()
     float_term:toggle()
+  end,
+  python = function()
+    python_term:toggle()
   end,
 }
