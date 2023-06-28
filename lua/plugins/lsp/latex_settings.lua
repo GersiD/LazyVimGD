aa82c329b542e0721cbd010c3bb85296bf6ed7ad
Gersi.doko <Gersi.doko@unh.edu>
2023-06-28 18:57:34 -0400

    customize latex for linux

--- lua/plugins/lsp/latex_settings.lua
+++ lua/plugins/lsp/latex_settings.lua
@@ -17,14 +17,28 @@ return {
   "lervag/vimtex",
   lazy = false,
   config = function()
-    vim.g.vimtex_compiler_latexmk = { build_dir = "build" }
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = "build",
+      aux_dir = "build",
+      -- out_dir = "build",
+    }
+    vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_quickfix_open_on_warning = false
     vim.g.vimtex_quickfix_ignore_filters = { "Underfull", "Overfull" }
-    return {
-      ["vimtex_view_general_viewer"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
-      ["vimtex_view_method"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
-      ["vimtex_view_general_options"] = "-reuse-instance -forward-search @tex @line @pdf",
-      ["vimtex_view_general_options_latexmk"] = "-reuse-instance",
-    }
+    if jit.os == "Windows" then
+      return {
+        ["vimtex_view_general_viewer"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
+        ["vimtex_view_method"] = "C:/Users/gersi/AppData/Local/Microsoft/WindowsApps/SumatraPDF.exe",
+        ["vimtex_view_general_options"] = "-reuse-instance -forward-search @tex @line @pdf",
+        ["vimtex_view_general_options_latexmk"] = "-reuse-instance",
+      }
+    else
+      return {
+        ["vimtex_view_general_viewer"] = "",
+        ["vimtex_view_method"] = "",
+        ["vimtex_view_general_options"] = "",
+        ["vimtex_view_general_options_latexmk"] = "",
+      }
+    end
   end,
 }
