242ba1060fb4c70586a937bc4c7e5d5758274a50
Gersi.doko <Gersi.doko@unh.edu>
2023-06-28 20:27:30 -0400

    adding highligh undo

---
+++ lua/plugins/highlight_undo.lua
@@ -0,0 +1,14 @@
+return {
+  "tzachar/highlight-undo.nvim",
+  keys = { "u", "<C-r>" },
+  config = function()
+    require("highlight-undo").setup({
+      hlgroup = "HighlightUndo",
+      duration = 300,
+      keymaps = {
+        { "n", "u", "undo", {} },
+        { "n", "<C-r>", "redo", {} },
+      },
+    })
+  end,
+}
--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -2,9 +2,11 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = "*.py",
   callback = function()
     require("null-ls").register({
-      require("null-ls").builtins.formatting.black,
+      -- require("null-ls").builtins.formatting.black,
+      -- require("null-ls").builtins.formatting.autopep8,
+      -- require("null-ls").builtins.formatting.yapf,
     })
-  end
+  end,
 })
 
 --TODO: - add debug adapter for python
