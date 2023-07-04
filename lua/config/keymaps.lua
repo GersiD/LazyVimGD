ca7bb42d5fed53b7fcda8ebf71f8592634bb6175
Gersi.doko <Gersi.doko@unh.edu>
2023-07-04 07:07:11 -0400

    changing quit to work with edgy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,7 +27,7 @@ end, { desc = "Python" })
 
 vim.api.nvim_set_keymap("v", "<leader>/", "gc", { desc = "Comment Selection" })
 vim.api.nvim_set_keymap("n", "<leader>/", "Vgc", { desc = "Comment Line" })
-vim.api.nvim_set_keymap("n", "q", ":q!<cr>", { desc = "Quit" })
+vim.api.nvim_set_keymap("n", "q", ":quitall<cr>", { desc = "Quit" })
 vim.api.nvim_set_keymap("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Quit" })
 
 -- Package manager keymaps
