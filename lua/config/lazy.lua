001ed75193c1abc8a5f4faa4703a8c39c557edaf
Gersi.doko <Gersi.doko@unh.edu>
2023-08-04 19:08:25 -0400

    adding some language packs to lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,9 @@ require("lazy").setup({
     { import = "plugins.lsp" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.java" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
