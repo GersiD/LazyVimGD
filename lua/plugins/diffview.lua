4fa97fc56641b907a7c6f246b5215d77b49ef567
Gersi.doko <Gersi.doko@unh.edu>
2023-08-07 10:04:57 -0400

    Make diffview look better

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,23 @@
 return {
   "sindrets/diffview.nvim",
   cmd = "DiffviewOpen",
+  opts = {
+    view = {
+      default = {
+        layout = "diff2_vertical",
+        winbar_info = true,
+      },
+      merge_tool = {
+        layout = "diff3_mixed",
+      },
+    },
+    keymaps = {
+      view = {
+        { "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close Diffview" } },
+      },
+      file_panel = {
+        { "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close Diffview" } },
+      },
+    },
+  },
 }
