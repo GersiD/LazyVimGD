e01fa5cdac0507c297ea77810b4e700487619ef8
Gersi.doko <Gersi.doko@unh.edu>
2023-08-13 20:51:44 -0400

    adding typescript

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tex" },
   },
   defaults = {
