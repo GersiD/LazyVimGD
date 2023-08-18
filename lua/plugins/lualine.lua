8f6c663f7b6926b72d07b8088bba413bad34002d
Gersi.doko <Gersi.doko@unh.edu>
2023-08-18 13:58:13 -0400

    updating lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,8 +5,8 @@ return {
     table.insert(opts.sections.lualine_x, { -- Lsp server name .
       function()
         local msg = ""
-        local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
-        if next(clients) == nil then
+        local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+        if vim.tbl_isempty(clients) then
           return "No Active Lsp"
         end
         for ind, client in ipairs(clients) do
@@ -24,17 +24,17 @@ return {
     -- fetch status of copilot_lua
     -- table.insert(opts.sections.lualine_x, {
     --   function()
-    --     local client = vim.lsp.get_active_clients({ name = "copilot" })[1]
+    --     local client = vim.lsp.get_clients({ name = "copilot" })[1]
     --     if client == nil or vim.tbl_isempty(client.requests) then
     --       return ""
     --     end
     --
-    --     -- local spinners = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
-    --     -- local ms = vim.loop.hrtime() / 1000000
-    --     -- local frame = math.floor(ms / 120) % #spinners
-    --     --
-    --     -- return "" .. spinners[frame + 1]
-    --     return ""
+    --     local spinners = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
+    --     local ms = vim.loop.hrtime() / 1000000
+    --     local frame = math.floor(ms / 120) % #spinners
+    --
+    --     return "" .. spinners[frame + 1]
+    --     -- return ""
     --   end,
     --   color = { fg = "#2ac3de" },
     -- })
