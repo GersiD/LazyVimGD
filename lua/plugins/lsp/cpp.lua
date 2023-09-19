cea265937029477884deb44a1ce1a524bbfa3a2f
git <gd1064@wildcats.unh.edu>
2023-09-18 20:04:59 -0400

    customize C++

--- lua/plugins/lsp/cpp.lua
+++ lua/plugins/lsp/cpp.lua
@@ -1,25 +1,24 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.cpp", "*.hpp" },
-  desc = "Setup C++ LSP",
-  callback = function()
-    vim.keymap.set("n", "<leader>`", function()
-      -- Save the current buffer
-      vim.cmd("w")
-      require("config.utils.terminals").run(
-        "make uncross && .\\uncross ./graphs/graph-complex.txt",
-        { direction = "horizontal" }
-      )
-    end, { desc = "Computer Graphics" })
-  end,
-  once = true,
-})
-
 return {
   "neovim/nvim-lspconfig",
   opts = {
     setup = {
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
+        vim.api.nvim_create_autocmd("BufEnter", {
+          pattern = { "*.cpp", "*.hpp" },
+          desc = "Setup C++ LSP",
+          callback = function()
+            vim.keymap.set("n", "<leader>`", function()
+              -- Save the current buffer
+              vim.cmd("w")
+              -- Read the compile_commands.json file in the current directory
+              local compile_commands = vim.fn.json_decode(vim.fn.readfile("compile_commands.json"))
+              local command = compile_commands and compile_commands[4]["run_command_custom"] or "make"
+              require("config.utils.terminals").run(command, { direction = "horizontal" })
+            end, { desc = "Computer Graphics" })
+          end,
+          once = true,
+        })
       end,
     },
   },
