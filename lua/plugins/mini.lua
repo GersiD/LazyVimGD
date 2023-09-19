ee4628c78406768a0835902c56d0c52598b7a8c5
Gersi.doko <Gersi.doko@unh.edu>
2023-09-18 20:04:25 -0400

    add md and replace mini.ai

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,18 @@
 return {
-  "echasnovski/mini.ai",
+  {
+    "echasnovski/mini.ai",
+    enabled = false,
+    keys = {
+      { "a", mode = { "x", "o" } },
+      { "i", mode = { "x", "o" } },
+    },
+    -- event = false,}
+  },
+  { "nvim-treesitter/nvim-treesitter-textobjects", requires = "nvim-treesitter/nvim-treesitter" },
   keys = {
     { "a", mode = { "x", "o" } },
     { "i", mode = { "x", "o" } },
+    { "[" },
+    { "]" },
   },
-  -- event = false,
 }
