c09213ba07194c7cf62a438966e61204c2cad4ae
Gersi.doko <Gersi.doko@unh.edu>
2023-09-18 21:06:31 -0400

    need mini.ai actually

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,11 @@
 return {
   {
+    -- Need this for ciq "change inner quotes"
     "echasnovski/mini.ai",
-    enabled = false,
     keys = {
       { "a", mode = { "x", "o" } },
       { "i", mode = { "x", "o" } },
     },
-    -- event = false,}
   },
   { "nvim-treesitter/nvim-treesitter-textobjects", requires = "nvim-treesitter/nvim-treesitter" },
   keys = {
