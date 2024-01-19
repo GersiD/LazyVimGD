4feb2c996aa18604230196fa7e7e9ac95f1ce7c6
Gersi.doko <Gersi.doko@unh.edu>
2024-01-19 12:24:05 -0500

    cant use null-ls in python

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -2,12 +2,6 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = "*.py",
   desc = "Setup Python DAP",
   callback = function()
-    require("null-ls").register({
-      -- require("null-ls").builtins.formatting.black,
-      -- require("null-ls").builtins.formatting.autopep8,
-      -- require("null-ls").builtins.formatting.yapf,
-    })
-
     -- Python Specific Keymaps
     -- run current file in terminal
     vim.keymap.set("n", "<leader><esc>", function()
